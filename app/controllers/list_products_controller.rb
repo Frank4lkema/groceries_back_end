73bb6b796ff4d18e1bb5577d863fd20a5c61b7c4
Frank Alkema <frank@aeroscan.nl>
2021-12-05 15:14:13 +0100

    Made the create action and tests

--- app/controllers/list_products_controller.rb
+++ app/controllers/list_products_controller.rb
@@ -1,6 +1,19 @@
+# frozen_string_literal: true
+
 class ListProductsController < ApplicationController
   def index
     list = List.find(params[:list_id])
     json_response(list.list_products)
+  end
+
+  def create
+    list_product = ListProduct.create!(list_product_params)
+    json_response(list_product,:created)
+  end
+
+  private
+
+  def list_product_params
+    params.permit(:amount, :product_id, :list_id)
   end
 end
--- spec/requests/list_products_spec.rb
+++ spec/requests/list_products_spec.rb
@@ -35,34 +35,34 @@ RSpec.describe 'List products API', type: :request do
     end
   end
 
-  # describe 'POST /lists' do
-  #   let(:valid_params) { { date: '06-12-2021'.to_date } }
-  #
-  #   context 'when request has valid params' do
-  #     before { post '/lists', params: valid_params }
-  #     it 'expected to create a new list-list' do
-  #       expect(json['date']).to eq('2021-12-06')
-  #     end
-  #
-  #     it 'expect to have code 201' do
-  #       expect(response).to have_http_status(201)
-  #     end
-  #   end
-  #
-  #   context 'when request has invalid params' do
-  #     let(:invalid_params) { { date: nil } }
-  #     before { post '/lists', params: invalid_params }
-  #
-  #     it 'returns status code 422' do
-  #       expect(response).to have_http_status(422)
-  #     end
-  #
-  #     it 'returns a validation failure message' do
-  #       expect(response.body)
-  #         .to match(/Validation failed: Date can't be blank/)
-  #     end
-  #   end
-  # end
+  describe 'POST /lists/:id/products' do
+    let(:new_product) { create(:product) }
+    context 'when request has valid params' do
+      let(:valid_params) { { amount: 1, product_id: new_product.id } }
+      before { post "/lists/#{list.id}/products", params: valid_params }
+      it 'expected to create a new list-product' do
+        expect(json['amount']).to eq(1)
+      end
+
+      it 'expect to have code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when request has invalid params' do
+      let(:invalid_params) { { amount: 0, product_id: new_product.id } }
+      before { post "/lists/#{list.id}/products", params: invalid_params }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Amount must be greater than 0/)
+      end
+    end
+  end
   #
   # # Test suite for DELETE /todos/:id
   # describe 'DELETE /lists/:id' do
